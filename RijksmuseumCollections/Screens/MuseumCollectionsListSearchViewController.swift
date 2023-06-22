e92e288829b64041c830dea47b557697f94d9103
Jayesh Kawli <j.kawli@gmail.com>
2023-06-22 12:29:20 +0200

    Added search bar handler and logic to hide and show empty search box message on home screen

--- RijksmuseumCollections/Screens/MuseumCollectionsListSearchViewController.swift
+++ RijksmuseumCollections/Screens/MuseumCollectionsListSearchViewController.swift
@@ -8,6 +8,31 @@
 import UIKit
 
 final class MuseumCollectionsListSearchViewController: UIViewController {
+
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search keyword for collections"
+        return searchBar
+    }()
+
+    private let emptySearchInputLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "Please start typing keyword in the search box to view the list of collections in Rijksmuseum"
+        return label
+    }()
+
+    private let emptySearchInputLabelParentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = Style.cornerRadius
+        view.backgroundColor = .lightGray
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,10 +49,52 @@ final class MuseumCollectionsListSearchViewController: UIViewController {
     }
 
     private func setupViews() {
-        self.view.backgroundColor = .white
+        view.backgroundColor = .white
+        title = "Rijksmuseum Collection"
+
+        searchBar.delegate = self
+
+        view.addSubview(searchBar)
+        view.addSubview(emptySearchInputLabelParentView)
+        emptySearchInputLabelParentView.addSubview(emptySearchInputLabel)
     }
 
     private func layoutViews() {
 
+        // Constraints for searchBar
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            emptySearchInputLabelParentView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptySearchInputLabelParentView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptySearchInputLabelParentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Style.Padding.defaultHorizontal),
+            emptySearchInputLabelParentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Style.Padding.defaultHorizontal)
+        ])
+
+        NSLayoutConstraint.activate([
+            emptySearchInputLabel.topAnchor.constraint(equalTo: emptySearchInputLabelParentView.topAnchor, constant: Style.Padding.smallVertical),
+            emptySearchInputLabel.bottomAnchor.constraint(equalTo: emptySearchInputLabelParentView.bottomAnchor, constant: -Style.Padding.smallVertical),
+            emptySearchInputLabel.leadingAnchor.constraint(equalTo: emptySearchInputLabelParentView.leadingAnchor, constant: Style.Padding.smallHorizontal),
+            emptySearchInputLabel.trailingAnchor.constraint(equalTo: emptySearchInputLabelParentView.trailingAnchor, constant: -Style.Padding.smallHorizontal),
+        ])
+    }
+}
+
+// MARK: UISearchBarDelegate
+extension MuseumCollectionsListSearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // We use this function to throttle trigger of requests since we don't want to fire multiple requests as user types too rapidly
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+            print(searchText)
+            if searchText.isEmpty {
+                self.emptySearchInputLabelParentView.isHidden = false
+            } else {
+                self.emptySearchInputLabelParentView.isHidden = true
+            }
+        }
     }
 }
---
+++ RijksmuseumCollections/Style/Style.swift
@@ -0,0 +1,22 @@
+//
+//  Style.swift
+//  RijksmuseumCollections
+//
+//  Created by Jayesh Kawli on 6/22/23.
+//
+
+import UIKit
+
+// A styles enum to store all the styles related code
+enum Style {
+
+    //MARK: Paddings
+    enum Padding {
+        static let defaultHorizontal: CGFloat = 16.0
+        static let defaultVertical: CGFloat = 16.0
+        static let smallVertical: CGFloat = 8.0
+        static let smallHorizontal: CGFloat = 8.0
+    }
+
+    static let cornerRadius: CGFloat = 8.0
+}
